d8d579248ae250c455d5293fc6407d9f311c9cca
Lerissa Mistry <149400509+lerissamistry@users.noreply.github.com>
2025-05-21 13:31:18 +0100

    Adding a new function

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 msg = "Hello, World!"
 print(msg)
 print("This is a test file.")
+
+def add(a, b):
+    return a + b
+
+a = 5
+b = 10
+
+result = add(a, b)
+print(f"The sum of {a} and {b} is {result}.")
